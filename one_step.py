f0b7af83dda10c0ddf0d8f028f9af05ee55bc844
jnobr <Janineobiri@gmail.com>
2026-03-05 14:43:03 +0000

    Updated one_step to handle repeated inputs and outputs

--- one_step.py
+++ one_step.py
@@ -3,7 +3,7 @@ import random
 from collections import Counter
 import math
 
-n = 256
+n = 1024 * 1024
 func = openlocationcode.encode
 outputs = []
 inputs = []
@@ -21,25 +21,29 @@ entropy_prob = []
 distribution_inputs = Counter(inputs)
 probabilities_inputs = []
 entropy_prob_inputs = []
-for i in outputs:
+
+for i in set(outputs):
+
     count = distribution.get(i)
     probability_i = count / n
     probabilities.append(probability_i)
     entropy_prob.append(probability_i * math.log2(probability_i))
 
-for i in inputs:
+
+for i in set(inputs):
+
     count = distribution_inputs.get(i)
     probability_j = count / n
     probabilities_inputs.append(probability_j)
     entropy_prob_inputs.append(probability_j * math.log2(probability_j))
 
 
-print(distribution)
-print(probabilities)
-print(entropy_prob)
-print(distribution_inputs)
-print(inputs)
-print(outputs)
+# print(distribution)
+# print(probabilities)
+# print(entropy_prob)
+# print(distribution_inputs)
+# print(inputs)
+# print(outputs)
 entropy = sum(entropy_prob)
 entropy_input = sum(entropy_prob_inputs)
 print(-entropy_input)
