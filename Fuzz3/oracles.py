77dd420b8714aa2379b7fdc26775f7bcace8055c
Karine EM <karineven79@gmail.com>
2026-03-13 19:05:57 +0000

    Implement DummyOracle function in oracles.py
    Added a dummy oracle function to print results.

--- Fuzz3/oracles.py
+++ Fuzz3/oracles.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
-# TODO
+def DummayOracle(results_map: dict[str, list[str]]):
+    for func_name, outputs in results_map.items():
+        print(f"Results for {func_name}: {outputs}")
+    return 0
+
