20c29d30ab17ce278386768a961f40b6d6d118b2
Karine EM <karineven79@gmail.com>
2026-03-13 15:01:40 +0000

    Update generator.py

--- Fuzz3/generator.py
+++ Fuzz3/generator.py
@@ -3,8 +3,18 @@ import subprocess
 import shlex
 import sys
 
-for i in range(n):
-    lat = random.randint(-90, 90)
-    long = random.randint(-180, 180)
-    inputs.append((lat, long))
-    outputs.append(func(lat, long))
+def olc_encode_executor(seedsno :int, outputfolder: Path, timeout: float) -> int:
+    total = 0
+    
+    # in a loop generate seeds
+    for i in range(n):
+        # check timeout
+
+        # generate
+        lat = random.randint(-90, 90)
+        long = random.randint(-180, 180)
+        inputs.append((lat, long))
+        outputs.append(func(lat, long))
+        # if ok ++ to total
+
+    return total
