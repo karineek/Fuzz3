6e4daca28078ff08f7c24c7a71fa403a55828d20
Karine EM <karineven79@gmail.com>
2026-03-13 19:40:34 +0000

    Update observers.py

--- Fuzz3/observers.py
+++ Fuzz3/observers.py
@@ -3,18 +3,18 @@ from pathlib import Path
 # Simple observer, just record the results for the third oracle
 def entropy_observer(input: str, sample: tuple[int, str, str]) -> list[str]: 
     # Use the function name itself to store the attribute
-    if not hasattr(olc_encoder_observer, "outputs"): 
-        olc_encoder_observer.inputs = []
-    if not hasattr(olc_encoder_observer, "outputs"): 
-        olc_encoder_observer.outputs = []
+    if not hasattr(entropy_observer, "outputs"): 
+        entropy_observer.inputs = []
+    if not hasattr(entropy_observer, "outputs"): 
+        entropy_observer.outputs = []
 
     # store data
-    olc_encoder_observer.inputs.append(input)
+    entropy_observer.inputs.append(input)
     # We just keep the valid answer, or if empty, the error!
     sample_data = sample[1] if sample[1] else sample[2]
-    olc_encoder_observer.outputs.append(sample_data)
+    entropy_observer.outputs.append(sample_data)
     
-    return olc_encoder_observer.inputs, olc_encoder_observer.outputs
+    return entropy_observer.inputs, entropy_observer.outputs
 
 
 
