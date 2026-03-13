fa9e10fce178035850c9fcbcb80fe1507377939c
Karine EM <karineven79@gmail.com>
2026-03-13 18:10:58 +0000

    Update generators.py

--- Fuzz3/generators.py
+++ Fuzz3/generators.py
@@ -4,9 +4,7 @@ import random
 def olc_encode_generator(seedsno: int, outputfolder: Path) -> int:
     outputfolder.mkdir(parents=True, exist_ok=True)
 
-    start = time.time()
     total = 0
-
     for i in range(seedsno):
         lat = random.randint(-90, 90)
         long = random.randint(-180, 180)
