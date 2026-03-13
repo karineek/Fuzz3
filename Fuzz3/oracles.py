600fc6da6a37b96d3ae46859b210b86ac1e9bc7d
Karine EM <karineven79@gmail.com>
2026-03-13 19:06:37 +0000

    Update oracles.py

--- Fuzz3/oracles.py
+++ Fuzz3/oracles.py
@@ -5,3 +5,37 @@ def DummayOracle(results_map: dict[str, list[str]]):
         print(f"Results for {func_name}: {outputs}")
     return 0
 
+distribution = Counter(outputs)
+probabilities = []
+entropy_prob = []
+distribution_inputs = Counter(inputs)
+probabilities_inputs = []
+entropy_prob_inputs = []
+
+for i in set(outputs):
+
+    count = distribution.get(i)
+    probability_i = count / n
+    probabilities.append(probability_i)
+    entropy_prob.append(probability_i * math.log2(probability_i))
+
+
+for i in set(inputs):
+
+    count = distribution_inputs.get(i)
+    probability_j = count / n
+    probabilities_inputs.append(probability_j)
+    entropy_prob_inputs.append(probability_j * math.log2(probability_j))
+
+
+# print(distribution)
+# print(probabilities)
+# print(entropy_prob)
+# print(distribution_inputs)
+# print(inputs)
+# print(outputs)
+entropy = sum(entropy_prob)
+entropy_input = sum(entropy_prob_inputs)
+print(-entropy_input)
+print(-entropy)
+print(len(distribution))
