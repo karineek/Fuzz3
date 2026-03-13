4e483e8d4c82320f6aa1a847fea006ea5f150e4f
Karine EM <karineven79@gmail.com>
2026-03-13 19:29:02 +0000

    Update oracles.py

--- Fuzz3/oracles.py
+++ Fuzz3/oracles.py
@@ -6,43 +6,31 @@ def DummayOracle(seed, results_map: dict[str, list[str]]):
     return 0
 
 def entropy_oracle(seed, results_map: dict[str, tuple[str, str]]):
-    data = results_map["shosos"]
-    for func_name, outputs in results_map.items():
-        if func_name in ["olc_encoder_observer","olc_encoder_observer"]
-        print(f"Results for {func_name}: {outputs}")
-        
-distribution = Counter(outputs)
-probabilities = []
-entropy_prob = []
+    data = results_map["entropy_observer"]
+    inputs = data[0]
+    outputs = data[1]
 
-distribution_inputs = Counter(inputs)
-probabilities_inputs = []
-entropy_prob_inputs = []
+    # Then the code as is!
+    distribution = Counter(outputs)
+    probabilities = []
+    entropy_prob = []
 
-for i in set(outputs):
+    distribution_inputs = Counter(inputs)
+    probabilities_inputs = []
+    entropy_prob_inputs = []
 
-    count = distribution.get(i)
-    probability_i = count / n
-    probabilities.append(probability_i)
-    entropy_prob.append(probability_i * math.log2(probability_i))
+    for i in set(outputs):
+        count = distribution.get(i)
+        probability_i = count / n
+        probabilities.append(probability_i)
+        entropy_prob.append(probability_i * math.log2(probability_i))
 
+    for i in set(inputs):
+        count = distribution_inputs.get(i)
+        probability_j = count / n
+        probabilities_inputs.append(probability_j)
+        entropy_prob_inputs.append(probability_j * math.log2(probability_j))
 
-for i in set(inputs):
-
-    count = distribution_inputs.get(i)
-    probability_j = count / n
-    probabilities_inputs.append(probability_j)
-    entropy_prob_inputs.append(probability_j * math.log2(probability_j))
-
-
-# print(distribution)
-# print(probabilities)
-# print(entropy_prob)
-# print(distribution_inputs)
-# print(inputs)
-# print(outputs)
-entropy = sum(entropy_prob)
-entropy_input = sum(entropy_prob_inputs)
-print(-entropy_input)
-print(-entropy)
-print(len(distribution))
+    entropy = sum(entropy_prob)
+    entropy_input = sum(entropy_prob_inputs)
+    return entropy_input, entropy, len(distribution)
