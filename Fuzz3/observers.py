d1ce325c1c2b8be79a0d6bf4eb44457d0f5966a9
Karine EM <karineven79@gmail.com>
2026-03-13 18:46:54 +0000

    Update observers.py

--- Fuzz3/observers.py
+++ Fuzz3/observers.py
@@ -1,4 +1,14 @@
 from pathlib import Path
 
-# TODO
+# Simple observer, just record the results for third oracle
+def olc_encoder_observer(sample: str) -> list[str]: 
+    # Use the function name itself to store the attribute
+    if not hasattr(olc_encoder_observer, "outputs"): 
+        olc_encoder_observer.outputs = []
+        
+    olc_encoder_observer.outputs.append(sample)
+    return olc_encoder_observer.outputs
+
+
+
 
