49d0b4e566a29f9963fd5f4ebd4ae8a181f52dd3
Karine EM <karineven79@gmail.com>
2026-03-13 19:12:57 +0000

    Update observers.py

--- Fuzz3/observers.py
+++ Fuzz3/observers.py
@@ -1,15 +1,20 @@
 from pathlib import Path
 
-# Simple observer, just record the results for third oracle
-def olc_encoder_observer(sample: tuple[int, str, str]) -> list[str]: 
+# Simple observer, just record the results for the third oracle
+def olc_encoder_observer(input, sample: tuple[int, str, str]) -> list[str]: 
     # Use the function name itself to store the attribute
+    if not hasattr(olc_encoder_observer, "outputs"): 
+        olc_encoder_observer.inputs = []
     if not hasattr(olc_encoder_observer, "outputs"): 
         olc_encoder_observer.outputs = []
 
+    # store data
+    olc_encoder_observer.inputs.append(input)
     # We just keep the valid answer, or if empty, the error!
     sample_data = sample[1] if sample[1] else sample[2]
     olc_encoder_observer.outputs.append(sample_data)
-    return olc_encoder_observer.outputs
+    
+    return olc_encoder_observer.inputs, olc_encoder_observer.outputs
 
 
 
