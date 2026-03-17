0808f84d97bca9811dc151963d31f32963d41de6
jnobr <Janineobiri@gmail.com>
2026-03-17 13:50:47 +0000

    Made some variable and function names more clear. Added olc encoder generator that uses illegal inputs

--- Fuzz3/generators.py
+++ Fuzz3/generators.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import random
 
-def olc_encoder_generator(seedsno: int, outputfolder: Path) -> int:
+
+def olc_encoder_generator_legal(seedsno: int, outputfolder: Path) -> int:
     outputfolder.mkdir(parents=True, exist_ok=True)
 
     total = 0
@@ -15,3 +16,20 @@ def olc_encoder_generator(seedsno: int, outputfolder: Path) -> int:
         total += 1
 
     return total
+
+
+def olc_encoder_generator_illegal(seedsno: int, outputfolder: Path) -> int:
+    for i in range(seedsno):
+        lat = random.randint(-1024, 1024)
+        long = random.randint(-1024, 1024)
+
+        seed_path = outputfolder / f"fuzz3_olc_{i}.seed"
+        seed_path.write_text(f"{lat},{long}\n")
+
+        total += 1
+
+    return total
+
+
+def olc_decoder_generator(seedsno: int, outputfolder: Path) -> tuple[int, int]:
+    pass
--- Fuzz3/oracles.py
+++ Fuzz3/oracles.py
@@ -2,23 +2,25 @@ from pathlib import Path
 from collections import Counter
 import math
 
-def DummayOracle(seed, results_map: dict[str, list[str]]):
+
+def DummyOracle(seed, results_map: dict[str, list[str]]):
     for func_name, outputs in results_map.items():
         print(f"Results for {func_name}: {outputs}")
     return 0
+
 
 def entropy_oracle(seed, results_map: dict[str, tuple[str, str]]):
     data = results_map["entropy_observer"]
     inputs = data[0]
     outputs = data[1]
     n = len(inputs)
-    if (n !=  len(outputs)):
-        return 0,0,0 
+    if n != len(outputs):
+        return 0, 0, 0
 
     # Then the code as is!
     distribution = Counter(outputs)
-    probabilities = []
-    entropy_prob = []
+    probabilities_output = []
+    entropy_prob_output = []
 
     distribution_inputs = Counter(inputs)
     probabilities_inputs = []
@@ -27,8 +29,8 @@ def entropy_oracle(seed, results_map: dict[str, tuple[str, str]]):
     for i in set(outputs):
         count = distribution.get(i)
         probability_i = count / n
-        probabilities.append(probability_i)
-        entropy_prob.append(probability_i * math.log2(probability_i))
+        probabilities_output.append(probability_i)
+        entropy_prob_output.append(probability_i * math.log2(probability_i))
 
     for i in set(inputs):
         count = distribution_inputs.get(i)
@@ -36,6 +38,6 @@ def entropy_oracle(seed, results_map: dict[str, tuple[str, str]]):
         probabilities_inputs.append(probability_j)
         entropy_prob_inputs.append(probability_j * math.log2(probability_j))
 
-    entropy = sum(entropy_prob)
+    entropy_output = sum(entropy_prob_output)
     entropy_input = sum(entropy_prob_inputs)
-    return -entropy_input, -entropy, len(distribution), n
+    return -entropy_input, -entropy_output, len(distribution), n
