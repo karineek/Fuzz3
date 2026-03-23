a6c5778bb9cbdac2e91e6d39761f91be3e1b2b29
Karine EM <karineven79@gmail.com>
2026-03-23 22:04:27 +0000

    Update observers.py

--- Fuzz3/observers.py
+++ Fuzz3/observers.py
@@ -22,7 +22,7 @@ def entropy_sliding_window_observer(input: str, sample: tuple[int, str, str]) ->
     if WINDOW_SIZE < 2:
         raise ValueError("Window size must be at least 2")
     else:
-        print(f">> (FUZZ3:Observer) Window size is {WINDOW_SIZE})
+        print(f">> (FUZZ3, Observer) Window size is {WINDOW_SIZE}")
         
     # Use the function name itself to store the attribute
     if not hasattr(entropy_sliding_window_observer, "inputs"):
