958885c29dc0825c71ab68553a7905eebf9a57f9
Karine EM <karineven79@gmail.com>
2026-03-23 21:37:17 +0000

    Update observers.py
    Adding sliding window

--- Fuzz3/observers.py
+++ Fuzz3/observers.py
@@ -1,4 +1,5 @@
-from pathlib import Path
+import os
+from collections import deque
 
 # Simple observer, just record the results for the third oracle
 def entropy_observer(input: str, sample: tuple[int, str, str]) -> list[str]: 
@@ -16,6 +17,22 @@ def entropy_observer(input: str, sample: tuple[int, str, str]) -> list[str]:
     
     return entropy_observer.inputs, entropy_observer.outputs
 
+WINDOW_SIZE = int(os.environ.get("ENTROPY_WINDOW_SIZE", "1024"))
+def entropy_sliding_window_observer(input: str, sample: tuple[int, str, str]) -> list[str]:
+    # Use the function name itself to store the attribute
+    if not hasattr(entropy_sliding_window_observer, "inputs"):
+        entropy_sliding_window_observer.inputs = deque(maxlen=WINDOW_SIZE)
+    if not hasattr(entropy_sliding_window_observer, "outputs"):
+        entropy_sliding_window_observer.outputs = deque(maxlen=WINDOW_SIZE)
+
+    # store data
+    entropy_sliding_window_observer.inputs.append(input)
+    # We just keep the valid answer, or if empty, the error!
+    sample_data = sample[1] if sample[1] else sample[2]
+    entropy_observer.outputs.append(sample_data)    
+    entropy_sliding_window_observer.outputs.append(sample_data)
+
+    return entropy_sliding_window_observer.inputs, entropy_sliding_window_observer.outputs
 
 
 
