95ff4049782afd09b7332c5bf817965d74bca34a
Karine EM <karineven79@gmail.com>
2026-03-26 15:04:12 +0000

    Update executors.py

--- Fuzz3/executors.py
+++ Fuzz3/executors.py
@@ -112,11 +112,8 @@ def clang_format_executor(
         )
         rc = 0 if result.returncode in [0, 1] else result.returncode
         ## print(f">>>> (executors) is {result.returncode} with RC is {rc}")
-        return code_in, rc,
-            output_formatter_clang_format(result.stdout.strip()),
-            output_formatter_clang_format(result.stderr.strip()) 
-            # Remove noise of the fuzzer file name 
-
+        return code_in, rc, output_formatter_clang_format(result.stdout.strip()), output_formatter_clang_format(result.stderr.strip()) 
+            
     except subprocess.TimeoutExpired as e:
         return code_in, 124, (e.stdout or "").strip(), (e.stderr or "timeout").strip()
 
