d9be30935bf9f8a888d1f156046759ca6829d9c5
Karine EM <karineven79@gmail.com>
2026-04-04 23:58:09 +0100

    Update script-compile-and-run.sh

--- C-compiler-AND-Utils/script-compile-and-run.sh
+++ C-compiler-AND-Utils/script-compile-and-run.sh
@@ -3,6 +3,6 @@ seed="${@: -1}"
 compiler_cmd="${@:1:$#-1}"
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-echo ">> RUN $compiler_cmd -x c -w $seed"
+# echo ">> RUN $compiler_cmd -x c -w $seed"
 $compiler_cmd -x c -w "$seed"
 ./a.out
