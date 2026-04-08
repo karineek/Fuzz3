2b5344e0c841ce02efd5fbdc7fa7108d99c498ad
Karine EM <karineven79@gmail.com>
2026-04-08 23:17:49 +0100

    Update fuzz_httpcore.sh

--- SUT/fuzz_httpcore.sh
+++ SUT/fuzz_httpcore.sh
@@ -1,4 +1,8 @@
-cd ..
-python3 blackbox.py -i httpcore/seeds -o httpcore/out -c httpcore/crashes --executor httpcore_executor \
+#!/bin/bash
+inseed=$1 # httpcore/seeds
+out=$2    # httpcore/out
+crash=$3  # httpcore/crashes
+itr=$4    # 10000
+python3 blackbox.py -i $inseed -o $out -c $crash --executor httpcore_executor \
                     --mutators bit_flip delete_line duplicate_line delete_char duplicate_char crazy_indentation add_one_mixed_tokens \
-                    --observers entropy_sliding_window_observer --oracles entropy_oracle --iterations 50000 > httpcode-output.50k.log 2>&1
+                    --observers entropy_sliding_window_observer --oracles entropy_oracle --iterations $itr > httpcode-output.$itr.log 2>&1
