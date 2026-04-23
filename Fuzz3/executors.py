63956e9ac099db02c920f563854261327f6d98c2
Karine EM <karineven79@gmail.com>
2026-04-23 10:23:51 +0100

    Update executors.py

--- Fuzz3/executors.py
+++ Fuzz3/executors.py
@@ -21,7 +21,7 @@ def script_executor(
     try:
         code_in = seed.read_text(encoding="utf-8")
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     arg_parsed = shlex.split(arguments)
     cmd = [*arg_parsed, str(seed)]
@@ -47,7 +47,7 @@ def httpcore_executor(
         s = seed.read_text(encoding="utf-8").strip()
         url = str(s)
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     code = "import httpcore;" f"a= httpcore.request('GET', '{url}').status;" "print(a)"
 
@@ -72,7 +72,7 @@ def triangle_executor(
         s = seed.read_text().strip()
         a, b, c = s.replace(",", " ").split()
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     cmd = ["flaky_triangle", a, b, c]
 
@@ -95,11 +95,10 @@ def triangle_executor(
 def c_compiler_executor(
     arguments: str, seed: Path, timeout: float
 ) -> tuple[str, int, str, str]:
-    
     if (not seed):
-        return "", 122, "", "Invalid Path"
+        return "", 300, "", "Invalid Path"
     elif not seed.is_file():
-       return "", 121, "", "Not a File"
+       return "", 300, "", "Not a File"
 
     return script_executor(f"{arguments} -x c", seed, timeout) # All good, it is a file + it ends with .c
 
@@ -135,7 +134,7 @@ def clang_format_executor(
     try:
         code_in = seed.read_text(encoding="utf-8")
     except UnicodeDecodeError as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     arg_parsed = shlex.split(arguments)
     cmd = ["clang-format", *arg_parsed, str(seed)]
@@ -166,7 +165,7 @@ def olc_encode_executor(
         lat = float(a)
         lng = float(b)
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     code = (
         "from openlocationcode import openlocationcode as olc;"
@@ -192,7 +191,7 @@ def olc_decode_executor(
     try:
         code_in = seed.read_text().strip()
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     code = (
         "from openlocationcode import openlocationcode as olc;"
@@ -226,7 +225,7 @@ def h3_encode_executor(
         lng = float(parts[1])
         res = int(arguments) if arguments else 10
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     code = "import h3;" f"print(h3.latlng_to_cell({lat},{lng},{res}))"
 
@@ -248,7 +247,7 @@ def h3_decode_executor(
     try:
         h3_index = seed.read_text().strip()
     except Exception as e:
-        return "", 125, "", "Invalid (Fuzz3)"
+        return "", 300, "", "Invalid (Fuzz3)"
 
     code = (
         "import h3;"
