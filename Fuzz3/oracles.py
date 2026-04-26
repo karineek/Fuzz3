d442596230a1a88547240794a175bfaae44fba3e
Karine EM <karineven79@gmail.com>
2026-04-26 16:41:18 +0100

    Update oracles.py

--- Fuzz3/oracles.py
+++ Fuzz3/oracles.py
@@ -3,7 +3,7 @@ from collections import Counter
 import math
 
 
-def DummyOracle(seed, results_map: dict[str, list[str]]):
+def dummy_oracle(seed, results_map: dict[str, list[str]]):
     for func_name, outputs in results_map.items():
         print(f"Results for {func_name}: {outputs}")
     return 0
