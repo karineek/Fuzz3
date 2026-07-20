04a87a41c870a24f8e95fcf5fe55e0e84fdd9297
Karine EM <karineven79@gmail.com>
2026-07-20 16:05:50 +0100

    Update executors.py

--- Fuzz3/executors.py
+++ Fuzz3/executors.py
@@ -58,7 +58,8 @@ def docker_executor(
         return "", 300, "", "Invalid (Fuzz3)"
 
     arg_parsed = shlex.split(arguments)
-    cmd = ["docker", "exec", "-it", DOCKER_IMAGE, "sh", "-lc", "'", *arg_parsed, str(seed), "'"]
+    shell_command = shlex.join([*arg_parsed, str(seed)])
+    cmd = ["docker", "exec", "-it", DOCKER_IMAGE, "sh", "-lc", shell_command]
     ## E.g. docker exec -it 10c3cd4d4526 sh -lc 'python3 /opt/test_ollama.py'
     try:
         result = subprocess.run(
