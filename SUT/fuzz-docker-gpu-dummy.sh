9007c5432767a62ce967779b8349670aee8b0ae6
Ubuntu <karineven79@gmail.com>
2026-07-20 16:33:14 +0100

    adding the data for starting fuzzing with gpu

--- SUT/fuzz-docker-gpu-dummy.sh
+++ SUT/fuzz-docker-gpu-dummy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-inseed=$1 # docker-in
+inseed=$1 # docker-in-gpu-dummy
 out=$2    # out
 crash=$3  # crashes
 itr=$4    # 10000
+
+export DOCKER_IMAGE=ded7e74f09a8
+
 python3 ../blackbox.py -i $inseed -o $out -c $crash \
         --executor docker_executor \
-        --executor-args "python3 /opt/test_ollama.py" \
+        --executor-args "python3 /fuzz_workspace/forkserver.py" \
         --mutators none \
         --observers entropy_sliding_window_observer \
         --oracles entropy_oracle \
