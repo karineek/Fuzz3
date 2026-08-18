d6a3afce9c3c49e1f9ea55d3ffbb621290fbf118
Karine EM <karineven79@gmail.com>
2026-08-18 11:23:33 +0100

    Update install-gpu-cloudlab.sh

--- install-gpu-cloudlab.sh
+++ install-gpu-cloudlab.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install -y linux-headers-$(uname -r) nvidia-driver-470-server
+sudo apt install nvidia-cuda-toolkit
 sudo reboot
 
 echo ">> Run: `nvidia-smi` to see the status of the GPUs."
